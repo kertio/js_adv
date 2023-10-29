8c5ad60882d208e2c545300fb7d7e8250d7d87ae
kertio <_root_@list.ru>
2023-10-29 20:00:47 +0500

    fix: Добавить обработку ошибок

--- 9-pokemon/js/script.js
+++ 9-pokemon/js/script.js
@@ -4,19 +4,27 @@ const req = new XMLHttpRequest();
 req.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
 req.send();
 
-req.addEventListener('load', function() {
-    const {abilities} = JSON.parse(this.responseText);
-    const {url} = abilities[0].ability;
-    
-    req.open('GET', url);
-    req.send();
-
-    req.addEventListener('load', function() {
-        const {effect_entries} = JSON.parse(this.responseText);
+req.addEventListener('load', function () {
+    if (this.status === 200) {
+        const {abilities} = JSON.parse(this.responseText);
+        const {url} = abilities[0].ability;
         
-        const {effect} = effect_entries.find(item => {
-            return item.language.name == 'en';
-        });
-        console.log(effect);
-    })
+        req.open('GET', url);
+        req.send();
+    
+        req.addEventListener('load', function () {
+            if (this.status === 200) {
+                const {effect_entries} = JSON.parse(this.responseText);
+                const {effect} = effect_entries.find(item => {
+                    return item.language.name == 'en';
+                });
+                console.log(effect);
+            } else {
+                console.warn('Ошибка получения описания абилки');        
+            }
+
+        })
+    } else {
+        console.warn('Ошибка получения списка абилок');
+    }
 })
